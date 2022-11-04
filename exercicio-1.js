4f5f5644debaa8d5e6a04cb5d847a2665e070a5c
jazmin pinto <jazminpinto61@gmail.com>
2022-11-04 19:21:57 -0300

    ejercicio da aula 2

---
+++ exercicio-1.js
@@ -0,0 +1,16 @@
+console.log ('ola do JS')
+console.warn ('mensagem de atençao')
+console.error ('mensagem de erro')
+/*
+console.log ('A message to you')
+console.table ( 
+    {
+    firstName: 'John'
+    lastName: 'Doe'
+}
+);
+*/
+
+/*comentario em bloco multilinhas*/
+//comentario em linha
+
