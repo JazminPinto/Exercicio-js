33818d805d0e742658beaed52ee61e4bd8a94ae5
jazmin pinto <jazminpinto61@gmail.com>
2022-11-04 21:36:43 -0300

    exercicio 2 da aula 5

---
+++ exercicio-2.js
@@ -0,0 +1,10 @@
+/* receber dados de mais de uma pessoa
+*partir do exercicio anterior
+*receber dados de mais de uma pessoa
+//utilizaremos o prompt apenas enquanto nao estamos alterando os elementos da pagina
+comparar e exibir mensagem comparando dados
+*comparar os dados recibidos e exibir mensagens
+//ex. jonh e mais velho que bob?
+      a cor preferida de maria e a mesma de bobo, azul
+      
+exibir mensagens personalizadas */
